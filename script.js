bc2b5966c6bba520793e6fbd4f9fcb36d1936fb6
RahulUbale <rahul.ubale19@gmail.com>
2025-09-02 14:25:19 -0400

    changes

---
+++ script.js
@@ -0,0 +1,4 @@
+// This function changes the text of the paragraph with id 'demo'
+function changeText() {
+  document.getElementById('demo').textContent = "Text changed by external JavaScript!";
+}
